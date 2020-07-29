a09873ffa9304e1cd274d0fbba9ce2be90378b41
Ricardo Jose Fellini <57669094+ricardofellini@users.noreply.github.com>
2020-07-29 17:06:46 -0300

    Objects Movement

---
+++ ObjectsMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] Positions;
+
+    int NextPosIndex;
+
+    Transform NextPos;
+
+    public float Speed;
+
+    void Start()
+    {
+        NextPos = Positions[0];
+    }
+
+    
+    void Update()
+    {
+        MoveGameObjects();
+    }
+
+    void MoveGameObjects()
+    {
+
+
+        if (transform.position == NextPos.position)
+        {
+
+            NextPosIndex++;
+            if(NextPosIndex >= Positions.Length)
+            {
+                NextPosIndex = 0;
+
+            }
+            NextPos = Positions[NextPosIndex];
+        }
+        else
+        {
+
+
+            transform.position = Vector3.MoveTowards(transform.position, NextPos.position, Speed * Time.deltaTime);
+        }
+    }
+    
+}

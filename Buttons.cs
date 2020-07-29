25c8b994c26cb35eac15cd392e70f7f0c147ce25
Ricardo Jose Fellini <57669094+ricardofellini@users.noreply.github.com>
2020-07-29 17:05:33 -0300

    Add files via upload
    Buttons Controller

---
+++ Buttons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    // button ExitGame
+    public void doExitGame()
+    {
+
+
+        Application.Quit();
+    }
+
+    // button play 
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene("Gameplay 1");
+
+    }
+
+    // button back to menu
+    public void BackMenu()
+    {
+
+        SceneManager.LoadScene("Menu");
+
+    }
+
+
+
+}
